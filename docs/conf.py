b5aefd71788c5177f044621b98c4fd6c5772d757
vanderlindenma <vanderlindenma@users.noreply.github.com>
2023-03-09 14:54:10 +0100

    pyproject.toml

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,12 @@ release = '2023'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "myst_parser",
+]
+
+source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,7 +51,6 @@ language = 'English'
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
